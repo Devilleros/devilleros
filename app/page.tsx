77801a562988ab45620bcc0fb9df3416e200ecf4
Devilleros <juandevilleros@gmail.com>
2025-04-16 14:50:59 -0500

    titulo

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <Image
-          src="/images/hero.png"
-          alt="Hero Image"
-          width={500}
-          height={500}
-        />
-        <p className="text-lg">Welcome to Devilleros!</p>
+        <h1 className="absolute top-1/2 left-1/2 z-10 w-full max-w-3xl
+         -translate-x-1/2 -translate-y-1/2 transform text-center font-mono
+          text-sm lg:flex lg:items-center lg:justify-center">
+          Bienvenido a devilleros.com
+        </h1>
       </main>
     </div>
   );
