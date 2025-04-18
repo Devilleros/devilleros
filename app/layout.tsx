a2222c38d09608988d20a27b688718119200b792
Devilleros <juandevilleros@gmail.com>
2025-04-18 11:13:40 -0500

    reset al ultimo commit que funciono en vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${pressStart.className} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${pressStart.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import Steam from "./components/stetic/Steam";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center ">
-      <Steam />
-      <h1 className="text-4xl mb-4 font-press">Bienvenido a devilleros.com</h1>
+      {/* <Steam /> */}
+      <h1 className="text-4xl mb-4">Bienvenido a devilleros.com</h1>
       <Link href="/inicio">
         <SquareChevronRight size={50} strokeWidth={1.5} />
       </Link>
