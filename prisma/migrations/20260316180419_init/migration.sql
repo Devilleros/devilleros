85436962d5e272f267b2a5b51e390949f639442c
Devilleros <juandevilleros@gmail.com>
2026-03-16 13:07:56 -0500

    16 mar: reestructuración del back

--- prisma/migrations/20260316180419_init/migration.sql
+++ prisma/migrations/20260316180419_init/migration.sql
@@ -21,6 +21,23 @@ CREATE TABLE "registros" (
     CONSTRAINT "registros_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "lastName" TEXT,
+    "image" TEXT,
+    "passwordHash" TEXT,
+    "provider" TEXT,
+    "providerId" TEXT,
+    "role" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE INDEX "registros_codigo_trabajador_idx" ON "registros"("codigo_trabajador");
 
@@ -29,6 +46,12 @@ CREATE INDEX "registros_hora_entrada_idx" ON "registros"("hora_entrada");
 
 -- CreateIndex
 CREATE INDEX "registros_created_at_idx" ON "registros"("created_at");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_providerId_provider_idx" ON "users"("providerId", "provider");
 
 -- AddForeignKey
 ALTER TABLE "registros" ADD CONSTRAINT "registros_codigo_trabajador_fkey" FOREIGN KEY ("codigo_trabajador") REFERENCES "trabajadores"("codigo") ON DELETE CASCADE ON UPDATE CASCADE;
